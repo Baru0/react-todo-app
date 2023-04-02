c5b88e5c76c9f50bddb41e01ca07e34349b826c7
Baru0 <89037916+Baru0@users.noreply.github.com>
2023-04-02 21:26:07 +0200

    Improvements

--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mysql = require('mysql')
+const fs = require('fs')
 const router = express.Router();
 require('dotenv').config();
 
+/* CONNECTION */
 
 console.log("Connecting to MySQL at", process.env.MYSQL_HOST)
 
@@ -11,17 +13,24 @@ const connection = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB_NAME,
-    // port: 1234
+    port: 3306, // default
+    ssl: {
+        ca: fs.readFileSync("./mysql-cert.pem")
+    }
 })
 
 let error = ""
 
 connection.connect((err) => {
-    console.log(err)
-    error = err
+    if(err) {
+        console.log(err)
+        error = err
+    } else {
+        console.log("Successfully connected.")
+    }
 })
 
-// https://www.npmjs.com/package/mysql
+/* UTILITY FUNCTIONS */
 
 const fallback = (responder) => {
     if(error) {
@@ -30,19 +39,23 @@ const fallback = (responder) => {
     }
 }
 
-const returner = (responder, queryResult) => {
+const returner = (responder, err, result) => {
     responder.json(
-        queryResult.error ?
-            { error: queryResult.error }
+        err ?
+            { err }
             :
-            queryResult.results
+            result
     )
 }
 
+/* API ENDPOINT DEFINITIONS */
 
 router.get('/todos', (req, res) => {
     if(fallback(res)) return
-    connection.query("SELECT * from todos", queryResult => returner(res, queryResult))
+
+    const op = "SELECT * FROM todos;"
+    console.log("Executing", op)
+    connection.query(op, (err, result) => returner(res, err, result))
 })
 
 router.post('/todos', (req,res) => {
@@ -52,13 +65,18 @@ router.post('/todos', (req,res) => {
             error: "The input field is empty"
         })
     }
-    connection.query(`INSERT INTO todos (action) VALUES ("${mysql.escape(req.body.action)}")`, queryResult => returner(res, queryResult))
+
+    const op = `INSERT INTO todos (action) VALUES (${mysql.escape(req.body.action)});`
+    console.log("Executing", op)
+    connection.query(op, (err, result) => returner(res, err, result))
 })
 
 router.delete('/todos/:id', (req, res) => {
     if(fallback(res)) return
-    connection.query(`DELETE _id, action FROM todos WHERE _id=${mysql.escape(req.params.id)}`, queryResult => returner(res, queryResult))
-})  
-  
+
+    const op = `DELETE FROM todos WHERE _id=${mysql.escape(req.params.id)};`
+    console.log("Executing", op)
+    connection.query(op, (err, result) => returner(res, err, result))
+})
 
 module.exports = router;
